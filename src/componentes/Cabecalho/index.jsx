c75083701597579bda839c54ff3c755f4e5b3d61
Hellen Assis <rm98284@fiap.com.br>
2023-09-28 18:41:57 -0300

    alterando barra lateral e criando banner

--- src/componentes/Cabecalho/index.jsx
+++ src/componentes/Cabecalho/index.jsx
@@ -5,11 +5,14 @@ const HeaderEstilizado = styled.header`
     padding: 60px 0;
     display: flex;
     justify-content: space-between;
+    img {
+        max-width: 212px;
+    }
 `
 
 const Cabecalho = () => {
-    return(<HeaderEstilizado>
-        <img src="/imagens/logo.png" alt="##" />
+    return (<HeaderEstilizado>
+        <img src="/imagens/logo.png" alt="" />
         <CampoTexto />
     </HeaderEstilizado>)
 }
